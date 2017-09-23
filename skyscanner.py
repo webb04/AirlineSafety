2e8af9f810ca642d1fb66e33bd5de6b8f085a12e
Jamie Webb <webb04@users.noreply.github.com>
2017-09-23 21:48:45 +0100

    Update skyscanner.py

--- skyscanner.py
+++ skyscanner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import requests
-
 API_KEY = ""
 
 
@@ -24,24 +23,6 @@ session_response = requests.post("http://business.skyscanner.net/apiservices/pri
 
 session_url = session_response.headers['Location']
 
-print("Session URL from Skyscanner: {}".format(session_url))
-
 pricing_response = _poll_live_pricing(session_url)
 while pricing_response.status_code != 200:
     pricing_response = _poll_live_pricing(session_url)
-
-
-
-# url = "https://api.private-beta-1.pusherplatform.com:443/apps/bf294293-08b7-4f18-8f61-44eefd49d19b/feeds/getting-started"
-# # https://dash.pusher.com/apps/bf294293-08b7-4f18-8f61-44eefd49d19b/feeds/inspector
-#
-# payload = "{\"items\":" + str(pricing_response.json()['Segments'][0]) + "}"
-#
-# response = requests.request("POST", url, data=payload)
-#
-# print(response.text)
-
-
-
-
-print(pricing_response.json()['Segments'])
